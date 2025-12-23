c7fc695c220d6f0a13b954db549e5f538d2ae541
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 22:20:56 +0100

    Refactor image display and add directions section
    Added layout for venue images and directions.

--- pages/apollo_victoria.py
+++ pages/apollo_victoria.py
@@ -10,4 +10,16 @@ Venue_Images =  [f'{path}/apollo_victoria_indoor.jpg',
                  f'{path}/apollo_victoria_map.png']
 
 
-st.image(Venue_Images[1], caption='Building', clamp=True)
+col1, col2 = st.columns([1,1])
+col1.image(Venue_Images[0], caption='Interior', clamp=True)
+col2.image(Venue_Images[1], caption='Building', clamp=True)
+
+st.write('## How to get here:')
+st.write(':performing_arts: Adress: 16 Denman St, London W1D 7DY')
+st.write(':round_pushpin: Tube Station: Victoria Station')
+st.write(':railway_car: via lines: Jubilee & Victoria (20 min)')
+st.write(':walking: 0.1 km, about 2 min')
+
+
+st.write('#### Find it on maps:')
+st.image(Venue_Images[2], caption='Location', clamp=True)
