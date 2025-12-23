a97eaf181080df3e7dd713a11c73005f633bb873
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 21:32:43 +0100

    Add progress bar and status text to Apollo Victoria

--- pages/apollo_victoria.py
+++ pages/apollo_victoria.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 st.markdown("# Apollo Victoria")
 st.sidebar.header("Apollo Victoria")
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
 
 path = Path(__file__).parent / "../data"
 Venue_Images =  [f'{path}/apollo_victoria_indoor.jpg',
