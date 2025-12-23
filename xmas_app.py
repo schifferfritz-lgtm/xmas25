167309cc404d20ea65d64d6e3c0bc98092367edd
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 21:16:44 +0100

    Remove login/logout functionality from xmas_app.py

--- xmas_app.py
+++ xmas_app.py
@@ -50,37 +50,14 @@ st.set_page_config(
     page_title="xmas2025", page_icon="🖼️", initial_sidebar_state="collapsed",layout='centered'
 )
 
-
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-def login():
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
-
-def logout():
-    if st.button("Log out"):
-        st.session_state.logged_in = False
-        st.rerun()
-
-login_page = st.Page(login, title="Log in", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
-
 apollo_victoria = st.Page(
     "pages/apollo_victoria.py", title="Apollo Victoria", icon=":material/dashboard:", default=True
 )
-
-if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "Account": [logout_page],
-            "Venues": [apollo_victoria],
-        }
-    )
-else:
-    pg = st.navigation([login_page])
-
+pg = st.navigation(
+    {
+        "Venues": [apollo_victoria],
+    }
+)
 pg.run()
 
 
