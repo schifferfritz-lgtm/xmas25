cdb4ebbb0e4e62aeaab2d6d6fe938572a1ad0993
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 21:59:49 +0100

    Update Picadilly_Theatre.py

--- pages/Picadilly_Theatre.py
+++ pages/Picadilly_Theatre.py
@@ -9,5 +9,10 @@ Venue_Images =  [f'{path}/picadilly_indoor.jpg',
                  f'{path}/picadilly_outdoor.jpeg',
                  f'{path}/picadilly_map.png']
 
+col1, col2 = st.columns([1,1])
+col1.image(Venue_Images[1], caption='Building', clamp=True)
+col2.image(Venue_Images[1], caption='Interior', clamp=True)
 
-st.image(Venue_Images[1], caption='Building', clamp=True)
+col1, col2 = st.columns([1,1])
+col1.image(Venue_Images[2], caption='Location', clamp=True)
+col2.write('How to get here:')
