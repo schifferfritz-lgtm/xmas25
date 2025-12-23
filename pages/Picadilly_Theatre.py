d7a332a8e14a358bf74f678e48d9b7c09060ec96
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 22:18:57 +0100

    Update address and transport information format

--- pages/Picadilly_Theatre.py
+++ pages/Picadilly_Theatre.py
@@ -14,10 +14,10 @@ col1.image(Venue_Images[0], caption='Interior', clamp=True)
 col2.image(Venue_Images[1], caption='Building', clamp=True)
 
 st.write('## How to get here:')
-st.write(':round_pushpin: Adress: 16 Denman St, London W1D 7DY')
-st.write(':round_pushpin::railway_car: Tube Station: Piccadilly Circus Underground Station')
-st.write(':railway_car: Tube Lines: Piccadilly')
-st.write(':walking: Walking Distance: 0.2 km, about 3 min')
+st.write(':performing_arts: Adress: 16 Denman St, London W1D 7DY')
+st.write(':round_pushpin: Tube Station: Piccadilly Circus Underground Station')
+st.write(':railway_car: via Tube Lines: Jubilee & Piccadilly (20 min)')
+st.write(':walking: 0.2 km, about 3 min')
 
 
 st.write('#### Find it on maps:')
