a90a5c8b4f259db87a65996e4dab7eeb5a2f4075
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 21:20:43 +0100

    Update image paths for Apollo Victoria venue

--- pages/apollo_victoria.py
+++ pages/apollo_victoria.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-Venue_Images =  ['xmas25/data/apollo_victoria_indoor.jpg',
-                 'xmas25/data/apollo_victoria_outdoor.jpg',
-                 'xmas25/data/apollo_victoria_map.png']
+Venue_Images =  ['../data/apollo_victoria_indoor.jpg',
+                 '../data/apollo_victoria_outdoor.jpg',
+                 '../data/apollo_victoria_map.png']
 
 
 image(Venue_Images[1], caption='Building', clamp=True)
