92815fe6d0e7a1e9ca9ba83e9a793596fddf2431
schifferfritz-lgtm <schifferfritz@gmail.com>
2025-12-23 22:08:52 +0100

    Update Picadilly_Theatre.py

--- pages/Picadilly_Theatre.py
+++ pages/Picadilly_Theatre.py
@@ -14,7 +14,12 @@ col1.image(Venue_Images[0], caption='Interior', clamp=True)
 col2.image(Venue_Images[1], caption='Building', clamp=True)
 
 st.write('## How to get here:')
+st.write(':round_pushpin: Adress: 16 Denman St, London W1D 7DY')
+st.write(':round_pushpin::railway_car: Tube Station: Piccadilly Circus Underground Station')
+st.write(':railway_car: Tube Lines: Piccadilly Circus Underground Station')
+st.write(':walking: Walking Distance: 0.2 km, about 3 min')
 
-st.write('## Find it on maps:')
+
+st.write('#### Find it on maps:')
 st.image(Venue_Images[2], caption='Location', clamp=True)
 
